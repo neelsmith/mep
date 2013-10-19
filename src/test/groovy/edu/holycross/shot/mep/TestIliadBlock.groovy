8fc5782bd6a1c05308a26b77a8eec212f4d4f934
Neel Smith <dnsmith.neel@gmail.com>
2013-10-18 22:08:09 -0400

    Add unit test for Iliad block retrieval.

---
+++ src/test/groovy/edu/holycross/shot/mep/TestIliadBlock.groovy
@@ -0,0 +1,22 @@
+package edu.holycross.shot.mep 
+
+import edu.harvard.chs.cite.CiteUrn
+import edu.harvard.chs.cite.CtsUrn
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+/**
+*/
+class TestIliadBlock extends GroovyTestCase {
+    // use default fuseki settings to test
+    def serverUrl = "http://localhost:3030/ds/"
+    MepGraph graph = new MepGraph(serverUrl)
+    CiteUrn twelverecto = new CiteUrn("urn:cite:hmt:msA.12r")
+
+    void testGraph() {
+        String expectedBlock = "0.0551,0.2305,0.5115,0.494"
+        assert graph.getIliadBlock(twelverecto.toString())  == expectedBlock
+    }
+
+}
